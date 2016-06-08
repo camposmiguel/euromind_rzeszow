8021436b4680ec850445cf91715a8d0bce04ea98
Miguel Campos <camposmiguel@gmail.com>
2016-06-08 17:34:54 +0200

    SOundPool

--- 07_GreenDAO/02_DuckHunt/app/src/main/java/com/miguelcr/duckhunt/MainActivity.java
+++ 07_GreenDAO/02_DuckHunt/app/src/main/java/com/miguelcr/duckhunt/MainActivity.java
@@ -2,6 +2,8 @@ package com.miguelcr.duckhunt;
 
 import android.graphics.Point;
 import android.graphics.Typeface;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
@@ -16,18 +18,22 @@ public class MainActivity extends AppCompatActivity {
     Random random;
     int counter = 0;
     TextView tvCounterDucks;
+    SoundPool soundEffects;
+    int duckSoundId;
+    AudioAttributes aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         duck = (ImageView)findViewById(R.id.duckImage);
         tvCounterDucks = (TextView) findViewById(R.id.countDucks);
 
         // Change the font type for the TextView tvCounterDucks
         Typeface type = Typeface.createFromAsset(getAssets(),"pixel.otf");
         tvCounterDucks.setTypeface(type);
+
+        configSound();
 
         randomDuckPosition();
     }
@@ -45,11 +51,31 @@ public class MainActivity extends AppCompatActivity {
         int randomY = random.nextInt(height-duck.getHeight() + 1);
         duck.setX(randomX);
         duck.setY(randomY);
+
+        soundEffects.play(duckSoundId,1,1,1,1,1);
+
     }
 
     public void duckClicked(View view) {
         counter++;
         tvCounterDucks.setText("Ducks: "+String.valueOf(counter));
         randomDuckPosition();
+    }
+
+    public void configSound() {
+
+        // Audio properties
+        aa = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+
+        soundEffects = new SoundPool.Builder()
+                .setMaxStreams(10)
+                .setAudioAttributes(aa)
+                .build();
+
+        // Load sound of duck
+        duckSoundId = soundEffects.load(this,R.raw.cuak,1);
     }
 }
