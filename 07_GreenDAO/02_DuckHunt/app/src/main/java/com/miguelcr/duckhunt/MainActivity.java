cb82f504cd07ded251f1a0af8dfa7783d2c54e64
Miguel Campos <camposmiguel@gmail.com>
2016-06-08 17:05:09 +0200

    DuckHunt

--- 07_GreenDAO/02_DuckHunt/app/src/main/java/com/miguelcr/duckhunt/MainActivity.java
+++ 07_GreenDAO/02_DuckHunt/app/src/main/java/com/miguelcr/duckhunt/MainActivity.java
@@ -1,17 +1,21 @@
 package com.miguelcr.duckhunt;
 
 import android.graphics.Point;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     ImageView duck;
     Random random;
+    int counter = 0;
+    TextView tvCounterDucks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         duck = (ImageView)findViewById(R.id.duckImage);
+        tvCounterDucks = (TextView) findViewById(R.id.countDucks);
+
+        // Change the font type for the TextView tvCounterDucks
+        Typeface type = Typeface.createFromAsset(getAssets(),"pixel.otf");
+        tvCounterDucks.setTypeface(type);
 
         randomDuckPosition();
     }
@@ -39,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void duckClicked(View view) {
+        counter++;
+        tvCounterDucks.setText("Ducks: "+String.valueOf(counter));
         randomDuckPosition();
     }
 }
