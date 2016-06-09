b6e9e3c75f7166f31bc3035dc625e23362d3ca47
Miguel Campos <camposmiguel@gmail.com>
2016-06-09 16:37:19 +0200

    NavigationView

--- 07_GreenDAO/02_DuckHunt/app/src/main/java/com/miguelcr/duckhunt/LoginActivity.java
+++ 07_GreenDAO/02_DuckHunt/app/src/main/java/com/miguelcr/duckhunt/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.miguelcr.duckhunt;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.media.AudioAttributes;
 import android.media.SoundPool;
@@ -8,14 +9,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String PREFS_NAME = "DuckPrefFile";
     TextView logoText;
     Button btnLogin;
     SoundPool soundEffects;
     int gunSoundId;
     AudioAttributes aa;
+    EditText nickname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,7 @@ public class LoginActivity extends AppCompatActivity {
 
         logoText = (TextView) findViewById(R.id.logoText);
         btnLogin = (Button) findViewById(R.id.btnLogin);
+        nickname = (EditText) findViewById(R.id.nickName);
 
         // Change the font type for the TextView logoText
         Typeface type = Typeface.createFromAsset(getAssets(),"pixel.otf");
@@ -35,10 +41,23 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void doLogin(View view) {
-        soundEffects.play(gunSoundId,1,1,1,1,1);
+        if(nickname.getText().toString().isEmpty()) {
+            Toast.makeText(this,"Write a nickname!",Toast.LENGTH_LONG).show();
+        } else {
+            // Save the nickname for the future
+            SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("nicknameUser", nickname.getText().toString());
 
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+            // Commit the edits!
+            editor.commit();
+
+
+            soundEffects.play(gunSoundId, 1, 1, 1, 1, 1);
+
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
     }
 
     public void configSound() {
--- 07_GreenDAO/02_DuckHunt/app/src/main/java/com/miguelcr/duckhunt/MainActivity.java
+++ 07_GreenDAO/02_DuckHunt/app/src/main/java/com/miguelcr/duckhunt/MainActivity.java
@@ -1,9 +1,13 @@
 package com.miguelcr.duckhunt;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.graphics.Point;
 import android.graphics.Typeface;
 import android.media.AudioAttributes;
 import android.media.SoundPool;
+import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
@@ -11,16 +15,20 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     ImageView duck;
     Random random;
     int counter = 0;
-    TextView tvCounterDucks;
+    TextView tvCounterDucks, nick, tvTimer;
     SoundPool soundEffects;
     int duckSoundId;
     AudioAttributes aa;
+    AlertDialog.Builder builder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +36,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         duck = (ImageView)findViewById(R.id.duckImage);
         tvCounterDucks = (TextView) findViewById(R.id.countDucks);
+        nick = (TextView) findViewById(R.id.nickName);
+        tvTimer = (TextView) findViewById(R.id.countDownTimer);
+
+        // Get the value of nickname
+        SharedPreferences settings = getSharedPreferences(LoginActivity.PREFS_NAME, 0);
+        String userNick = settings.getString("nicknameUser", "");
+        nick.setText(userNick);
 
         // Change the font type for the TextView tvCounterDucks
         Typeface type = Typeface.createFromAsset(getAssets(),"pixel.otf");
         tvCounterDucks.setTypeface(type);
+        nick.setTypeface(type);
+        tvTimer.setTypeface(type);
 
         configSound();
 
         randomDuckPosition();
+
+        startCountDown();
+
+        alertGameOver();
+    }
+
+    private void alertGameOver() {
+        builder = new AlertDialog.Builder(this);
+        // Add the buttons
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                counter = 0;
+                startCountDown();
+            }
+        });
+        builder.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+                finish();
+            }
+        });
+        // Set other dialog properties
+
+
+        // Create the AlertDialog
+        AlertDialog dialog = builder.create();
+    }
+
+    private void startCountDown() {
+        //1 minute = 60000 milliseconds
+        new CountDownTimer(5000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                tvTimer.setText("00:" + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                tvTimer.setText("Game over!");
+                builder.setTitle("Game Over! Yo have hunted "+counter+" ducks. " +
+                        "Do you want to play again?");
+                builder.show();
+            }
+        }.start();
+
     }
 
     private void randomDuckPosition() {
